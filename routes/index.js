f3df2a10eca7d933aff082ad7168cbb30a314a24
tingul4 <40429714+tingul4@users.noreply.github.com>
2023-06-29 14:41:22 -0400

    fix: be able to sort in search page and home page

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,8 @@ const router = express.Router()
 
 const home = require('./modules/home.js')
 const restaurants = require('./modules/restaurants.js')
-const sort = require('./modules/sort.js')
 
 router.use('/', home)
 router.use('/restaurants', restaurants)
-router.use('/sort', sort)
 
 module.exports = router
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -4,19 +4,31 @@ const Restaurant = require('../../models/restaurant')
 const router = express.Router()
 
 router.get('/', (req, res) => {
+  const type = req.query.type
+  const sort = req.query.sort
+
   return Restaurant.find()
           .lean()
-          .sort({ _id: 'asc'})
+          .sort({ [type]: sort })
           .then(restaurants => res.render('index', { restaurants }))
           .catch(error => console.error(error))
 })
 
 router.get('/search', (req, res) => {
   const keyword = req.query.keyword
-  
+  const type = req.query.type
+  const sort = req.query.sort
+
+  if (!keyword)
+    return Restaurant.find()
+          .lean()
+          .sort({ [type]: sort })
+          .then(restaurants => res.render('index', { restaurants }))
+          .catch(error => console.error(error)) 
+
   return Restaurant.find()
           .lean()
-          .sort({ _id: 'asc'})
+          .sort({ [type]: sort })
           .then(restaurants => {
             return restaurants.filter(restaurant => {
               return restaurant.name.toLowerCase().includes(keyword.toLowerCase()) || restaurant.category.toLowerCase().includes(keyword.toLowerCase())
