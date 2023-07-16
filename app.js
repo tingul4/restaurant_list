b3dcf1861764e5e7ea972de3b3c0e09fd6072977
tingul4 <40429714+tingul4@users.noreply.github.com>
2023-07-16 14:50:35 -0400

    feat: add success and warning message

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const methodOverride = require('method-override')
 const routes = require('./routes')
 const session = require('express-session')
 const usePassport = require('./config/passport')
+const flash = require('connect-flash')
 require('./config/mongoose.js')
 
 if (process.env.NODE_ENV !== 'production') {
@@ -25,10 +26,13 @@ app.use(session({
 app.use(bodyParser.urlencoded({ extended : true }))
 app.use(express.static('public')) // setting static files
 app.use(methodOverride('_method'))
+app.use(flash())
 usePassport(app)
 app.use((req, res, next) => {
   res.locals.isAuthenticated = req.isAuthenticated()
   res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')  // 設定 success_msg 訊息
+  res.locals.warning_msg = req.flash('warning_msg')  // 設定 warning_msg 訊息
   next()
 })
 app.use(routes)
