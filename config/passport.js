6a3fe74b5d9b285ef87c34f7ca16337e31c2bd24
tingul4 <40429714+tingul4@users.noreply.github.com>
2023-07-16 17:01:54 -0400

    feat: add warning message when login fails

--- config/passport.js
+++ config/passport.js
@@ -9,14 +9,16 @@ module.exports = app => {
   app.use(passport.initialize())
   app.use(passport.session())
   // 設定本地登入策略
-  passport.use(new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
+  passport.use(new LocalStrategy({ usernameField: 'email', passReqToCallback: true }, (req, email, password, done) => {
     User.findOne({ email })
       .then(user => {
         if (!user) {
+          req.flash('warning_msg', '該 Email 未被註冊過！')
           return done(null, false, { message: 'That email is not registered!' })
         }
         return bcrypt.compare(password, user.password).then(isMatch => {
           if (!isMatch) {
+            req.flash('warning_msg', 'Email 或密碼錯誤！')
             return done(null, false, { message: 'Email or Password incorrect.' })
           }
           return done(null, user)
