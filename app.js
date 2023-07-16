6d329b8356c69bdfbe1b5a498fc3e1bc746eed5c
tingul4 <40429714+tingul4@users.noreply.github.com>
2023-07-16 10:04:44 -0400

    feat: create login and register function

--- app.js
+++ app.js
@@ -3,21 +3,37 @@ const bodyParser = require('body-parser')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const routes = require('./routes')
+const session = require('express-session')
+const usePassport = require('./config/passport')
 require('./config/mongoose.js')
 
-const port = 3000
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+const PORT = process.env.PORT
 const app = express()
 
 // setting template engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true
+}))
 app.use(bodyParser.urlencoded({ extended : true }))
 app.use(express.static('public')) // setting static files
 app.use(methodOverride('_method'))
+usePassport(app)
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  next()
+})
 app.use(routes)
 
 // start and listen on the Express server
-app.listen(port, () => {
-  console.log(`Express is listening on localhost:${port}`)
+app.listen(PORT, () => {
+  console.log(`Express is listening on localhost:${PORT}`)
 })
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const router = express.Router()
 
-const home = require('./modules/home.js')
-const restaurants = require('./modules/restaurants.js')
+const home = require('./modules/home')
+const restaurants = require('./modules/restaurants')
+const users = require('./modules/users')
 
 router.use('/', home)
 router.use('/restaurants', restaurants)
+router.use('/users', users)
 
 module.exports = router
