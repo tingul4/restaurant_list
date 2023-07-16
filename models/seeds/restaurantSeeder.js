664ba03d227ca452669facdc171005bdf92decc7
tingul4 <40429714+tingul4@users.noreply.github.com>
2023-07-16 16:36:23 -0400

    feat: build a seed file

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,23 +1,44 @@
-const mongoose = require('mongoose')
-const Restaurant = require('../restaurant') // load Restaurant schema
-const restaurant_data = require('../../restaurant.json')
-
+const bcrypt = require('bcryptjs')
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
+const Restaurant = require('../restaurant')
+const User = require('../user')
+const db = require('../../config/mongoose')
+const restaurant_list = require('./restaurant.json').results
+const SEED_USER = [
+  {
+    name: 'USER1',
+    email: 'user1@example.com',
+    password: '12345678'
+  },
+  {
+    name: 'USER2',
+    email: 'user2@example.com',
+    password: '12345678'
+  }
+]
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-const db = mongoose.connection
-db.on('error', () => {
-  console.log('mongodb error!')
-})
 db.once('open', () => {
-  console.log('mongodb connected!')
-})
-
-db.once('open', () => {
-  console.log('mongodb connected!')
-  // load data from restaurant.json
-  restaurant_data.results.forEach(obj => Restaurant.create(obj))
-  console.log('done')
+  return Promise.all(Array.from(SEED_USER, (seedUser, index) => {
+    return bcrypt
+      .genSalt(10)
+      .then(salt => bcrypt.hash(seedUser.password, salt))
+      .then(hash => User.create({
+        name: seedUser.name,
+        email: seedUser.email,
+        password: hash
+      }))
+      .then(user => {
+        const userId = user._id
+        return Promise.all(Array.from(
+          { length: 3 },
+          (_, i) => Restaurant.create({ ...restaurant_list[index * 3 + i], userId })
+        ))
+      })
+  }))
+  .then(() => {
+    console.log('done.')
+    process.exit()
+  })
 })
